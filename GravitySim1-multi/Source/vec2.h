47f6f25322e4826d00456791826a645833406fe8
etteerr <e.j.diepgrond@gmail.com>
2016-10-14 20:28:17 +0200

    Fixed passing reference to local

--- GravitySim1-multi/Source/vec2.h
+++ GravitySim1-multi/Source/vec2.h
@@ -118,7 +118,7 @@ public:
 		return (v1.x * v2.y) - (v1.y * v2.x);
 	}
 
-	vec2& direction(vec2& that) {
+	vec2 direction(vec2& that) {
 		vec2<double> direction;
 		double r = dist(that);
 		direction.x = (that.x - x)/r;
